16bb9e7f42091b1e74ba765f1466bad158e5412e
João Divino <joaodivinod@gmail.com>
2021-05-02 15:06:31 -0300

    Componente CountDown finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,33 @@
-import Head from "next/head";
 import {CompletedChallends} from "../components/CompletedChallends";
 import {ExperienceBar} from "../components/ExperienceBar";
 import {Profile} from "../components/Profile";
 
 import styles from '../styles/pages/Home.module.css'
+import {Countdown} from "../components/Countdown";
+
+import Head from "next/head";
+
 
 export default function Home() {
     return (
         <div className={styles.container}>
+
+            <Head>
+                <title>Inicio Move Yourself</title>
+            </Head>
+
             <ExperienceBar />
 
             <section>
-                <Profile/>
-                <CompletedChallends/>
-            </section>
+                <div>
+                    <Profile />
+                    <CompletedChallends />
+                    <Countdown />
+                </div>
+                <div>
 
+                </div>
+            </section>
         </div>
     )
 }
