d57b39bdf33db284500b57e1561a2802ac269c6e
João Divino <joaodivinod@gmail.com>
2021-05-03 23:20:19 -0300

    WIP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/global.css'
+
+import {ChallengensContext} from "../contexts/ChallengensContext";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ChallengensContext.Provider value={''}>
+        <Component {...pageProps} />
+      </ChallengensContext.Provider>
+  )
 }
 
 export default MyApp
